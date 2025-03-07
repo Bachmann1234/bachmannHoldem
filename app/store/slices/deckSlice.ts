d273a7d71e8ac4471bd1d79d705792165c5dc5d2
Matt Bachmann <bachmann.matt@gmail.com>
2025-03-07 14:23:57 -0500

    add prettier

--- app/store/slices/deckSlice.ts
+++ app/store/slices/deckSlice.ts
@@ -1,36 +1,36 @@
-import {createSlice} from "@reduxjs/toolkit";
-import { Card, Rank, Suit } from "../../types/Card";
+import { createSlice } from '@reduxjs/toolkit';
+import { Card, Rank, Suit } from '../../types/Card';
 
 interface DeckState {
-    cardStack: Card[];
+  cardStack: Card[];
 }
 
 const initialState: DeckState = {
-    cardStack: shuffleDeck(generateDeck())
-}
+  cardStack: shuffleDeck(generateDeck()),
+};
 
 export const deckSlice = createSlice({
-    name: 'deck',
-    initialState,
-    reducers: {
-        shuffle: (state: DeckState) => {
-            state.cardStack = shuffleDeck(state.cardStack);
-        },
-        drawCard: (state: DeckState) => {
-            state.cardStack.shift();
-        },
-        addCard: (state: DeckState, action: {payload: Card}) => {
-            state.cardStack.push(action.payload);
-        }
-    }
+  name: 'deck',
+  initialState,
+  reducers: {
+    shuffle: (state: DeckState) => {
+      state.cardStack = shuffleDeck(state.cardStack);
+    },
+    drawCard: (state: DeckState) => {
+      state.cardStack.shift();
+    },
+    addCard: (state: DeckState, action: { payload: Card }) => {
+      state.cardStack.push(action.payload);
+    },
+  },
 });
 
-function shuffleDeck(cardsStack: Card[]) : Card[] {
-    return cardsStack.sort(() => Math.random() - 0.5);
+function shuffleDeck(cardsStack: Card[]): Card[] {
+  return cardsStack.sort(() => Math.random() - 0.5);
 }
 
-function generateDeck() : Card[] {
-    const suits: Suit[] = Object.values(Suit);
-    const ranks: Rank[] = Object.values(Rank).filter(value => typeof value === 'number') as Rank[];
-    return suits.flatMap(suit => ranks.map(rank => ({suit, rank})));
+function generateDeck(): Card[] {
+  const suits: Suit[] = Object.values(Suit);
+  const ranks: Rank[] = Object.values(Rank).filter((value) => typeof value === 'number') as Rank[];
+  return suits.flatMap((suit) => ranks.map((rank) => ({ suit, rank })));
 }
